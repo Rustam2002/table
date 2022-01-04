b949c5b21782bdc34f3dd5d6da628ae5b2718a24
Rustam2002 <rustabdimelik@gmail.com>
2022-01-04 13:32:22 +0600

    start

---
+++ script.js
@@ -0,0 +1,47 @@
+$('.filters ul li').each(function(){
+    $(this).on("click", function(){
+        if ($(this).hasClass('milk')) {
+            $(".fl-ser").removeClass('active');
+            $(".fl-ser.milk").addClass('active');
+        }else if ($(this).hasClass('cheese')) {
+            $(".fl-ser").removeClass('active');
+            $(".fl-ser.cheese").addClass('active');
+        }else if ($(this).hasClass('syrok')) {
+            $(".fl-ser").removeClass('active');
+            $(".fl-ser.syrok").addClass('active');
+        }else if ($(this).hasClass('cto')) {
+            $(".fl-ser").removeClass('active');
+            $(".fl-ser.cto").addClass('active');
+        }
+        $('.filters').removeClass('active'); 
+        $(this).addClass('active');
+    });
+});
+$('#header ul li').each(function(){
+    $(this).on("click", function(){
+        if ($(this).hasClass('product')) {
+            $(".list").removeClass('activel');
+            $(".list.product").addClass('activel');
+            $(".w-product").addClass('w-active');
+            $(".w-zakaz").removeClass('w-active');
+            $(".w-registration").removeClass('w-active');
+        }else if ($(this).hasClass('zakaz')) {
+            $(".list").removeClass('activel');
+            $(".list.zakaz").addClass('activel');       
+            $(".w-zakaz").addClass('w-active');
+            $(".w-product").removeClass('w-active');       
+            $(".w-registration").removeClass('w-active');       
+        }else if ($(this).hasClass('registr')) {
+            $(".list").removeClass('activel');
+            $(".list.registr").addClass('activel');
+            $(".w-registration").addClass('w-active');
+            $(".w-product").removeClass('w-active');       
+            $(".w-zakaz").removeClass('w-active');  
+        }else if ($(this).hasClass('author')) {
+            $(".list").removeClass('activel');
+            $(".list.author").addClass('activel');
+        }
+        $('#header').removeClass('activle'); 
+        $(this).addClass('activel');
+    });
+});
